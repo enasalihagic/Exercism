76bdfbbdf20b3405a3c85c2d792905762fc67cb6
enasalihagic <enasalihagic@gmail.com>
2026-03-01 16:28:57 +0100

    Inclusion of udemy training practices

---
+++ udemy_practices/basic-io/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+	
+	//cout<<"Hello World"<<endl;
+	//cout<<"Hello";
+	//cout<<"World"<<endl;
+	
+	//cout << "Hello World" << endl; 
+	//cout << "Hello" << " world" << endl;
+	//cout << "Hello" << " world\n";
+	//cout << "Hello\nOut\nThere\n";
+	
+	int num1; 
+	int num2; 
+	double num3; 
+	
+	/*cout << "Enter an integer: "; 
+	cin >> num1; 
+	cout << "You entered: " << num1 << endl; */
+	
+ /*	 cout << "Enter a first integer: ";
+	 cin >> num1; 
+	 
+	 cout << "Enter a second integer: ";
+	 cin >> num2; 
+	 */
+	// cout << "Enter two integers separated with the space: "; 
+	// cin >> num1 >> num2; 
+	 
+	// cout << "You entered " << num1 << " and " << num2 << endl; 
+	
+ /*	cout << "Enter a double: ";
+	cin >> num3;
+ */
+ 
+	cout << "Enter an integer: ";
+	cin >> num1; 
+	
+	cout << "Enter a double: "; 
+	cin >> num3; 
+	
+	cout << "The integer is: " << num1 << endl;
+	cout << "The double is: " << num3 << endl;
+	return 0;
+}
